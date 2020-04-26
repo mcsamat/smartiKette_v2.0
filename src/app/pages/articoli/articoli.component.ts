e1dceec92fa76f780aa7bb7122707619857166a0
mcsamat <mcsamat@users.noreply.github.com>
2020-04-26 02:09:01 +0200

    Delete Item
    Aggiunta la funzione deleteItem() per eliminare un Articolo, con l'aggiunta di una conferma tramite Modal.

--- src/app/pages/articoli/articoli.component.ts
+++ src/app/pages/articoli/articoli.component.ts
@@ -1,14 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { ResourceLoader } from '@angular/compiler';
+import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
-var items_tot: number;
+var ditemm;
 
+// Modal Conferma DELETE Item - Template
+@Component({
+  selector: 'ngbd-modal-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Elimina Articolo</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>Sicuro di voler elimare questo articolo?</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-danger" (click)="deleteItem()">Sì, elimina</button>
+      <button type="button" class="btn btn-outline-primary" (click)="activeModal.close('Close click')">Annulla</button>
+    </div>
+  `
+})
+// Modal Conferma DELETE Item - Funzione
+export class NgbdModalContent {
+  @Input() name;
+  // Root URL per API
+  readonly ROOT_URL = 'http://pietro-test.dlinkddns.com:10082/api/item';
+
+  constructor(private httpClient: HttpClient, public activeModal: NgbActiveModal) {}
+  deleteItem(): void {
+    // Header apikey + Content-Type
+    let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+    headers.set('Content-Type', 'application/x-www-form-urlencoded');
+    // API - DELETE
+    this.httpClient.delete(this.ROOT_URL + '/concrete/' + ditemm.id, { headers }).subscribe(res =>
+      window.location.reload()
+      );
+  }
+}
+
+
+// Componente principale
 @Component({
   selector: 'app-articoli',
   templateUrl: './articoli.component.html',
   styleUrls: ['./articoli.component.scss']
 })
+
+
 export class ArticoliComponent implements OnInit {
   // Root URL per API
   readonly ROOT_URL = 'http://pietro-test.dlinkddns.com:10082/api/item';
@@ -18,17 +61,11 @@ export class ArticoliComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject();
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     // Header generale
     let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
-
-    // Trovo il numero totale di items
-    this.httpClient.get(this.ROOT_URL + '/concrete', { headers })
-    .toPromise().then((data:any) => {
-      items_tot = data.total_items;
-    });
 
     // DataTables
     this.dtOptions = {
@@ -36,17 +73,23 @@ export class ArticoliComponent implements OnInit {
       pageLength: 5,
       processing: true
     };
-    this.httpClient.get(this.ROOT_URL + '/concrete?recordsPerPage=999999999999' + items_tot, { headers })
+    this.httpClient.get(this.ROOT_URL + '/concrete?recordsPerPage=999999999999', { headers })
     .toPromise().then((data:any) => {
       // Passo i valori presi da API
       this.items$ = data.items;
       this.dtTrigger.next();
-      console.log(items_tot)
     });
   }
 
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
+  }
+  
+  // DELETE item
+  open(ditem: String) {
+    ditemm = ditem;
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.name = 'Conferma Eliminazione';
   }
 
 }
