987f1f1ab84b24196558c4aabffe668d6b234194
mcsamat <mcsamat@users.noreply.github.com>
2020-04-30 18:59:03 +0200

    Dashboard Final
    Fix della dashboard, miglioramento formattazione e funzioni, modifica al layou dei primi 6 box (rimosso lo spazio tra le righe)

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { System } from '../../variables/system';
-import { Identifiers } from '@angular/compiler';
+import { Global } from '../../variables/global';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,125 +9,131 @@ import { Identifiers } from '@angular/compiler';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  // Root URL per API
-  readonly ROOT_URL = 'http://pietro-test.dlinkddns.com:10082/api';
+  // Var Installazione
+  varInstallation;
+  // Var Display&Etichette
+  varLabelStat;
+  varTotMatch;
+  // Var APs
+  varAps;
+  // Var Articoli
+  varItem;
+  varTotItem;
+  // Var Importazioni
+  varImport;
+  // Var Mobile Device
+  varMobile;
+  
   // Variabili System
   system_info = new System('', '', '', '', '',  '',  '',  '',  '',  '',  '');
-  // Variabili mobile
-  mobiles;
-  // Variabili APs
-  aps;
-  taps: number;
-  // Variabili ItemType
-  items_t;
-  titems_t: number;
-  // Variabili Import
-  timports: number;
-  imports;
-  // Variabili labelStat - diventeranno una classe
-  onl:    number;
-  offl:   number;
-  bgood:  number;
-  bbad:   number;
-  // Variabili Configurazione - diventeranno una classe
-  conftype:     string;
-  confname:     string;
-  confpath:     string;
-  confinstall:  string;
-  // Var total item e total match attivi
-  titem: number;
-  tmatch: number;
-
   // Var Templates via ItemType.ID
   templates;
 
   
   constructor(private httpClient: HttpClient) { }
 
-  ngOnInit() {
-    // Header generale
-    let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+    ngOnInit() {
+      // Richiamo le funzioni per API
+      this.getInstallation();
 
-    // System API
-    this.httpClient.get(this.ROOT_URL + '/system', { headers })
-    .toPromise().then((data:any) => {
-      // Passo i valori presi da API
-      this.system_info = data;
-    });
+      this.getDisplay();
+      this.getTotalMatch();
+      this.getAps();
 
-   // Display API (labelinfo/statistics)
-   this.httpClient.get(this.ROOT_URL + '/labelinfo/statistics', { headers })
-    .toPromise().then((labelstat:any) => {
-      // Passo i valori presi da API
-      this.onl    = labelstat.connection.online;
-      this.offl   = labelstat.connection.offline;
-      this.bgood  = labelstat.battery.good;
-      this.bbad   = labelstat.battery.bad;
-    });
+      this.getItems();
+      this.getTotItem();
+      this.getImport();
 
-    // Config API
-    this.httpClient.get(this.ROOT_URL + '/system/configuration', { headers })
-    .toPromise().then((infoconf:any) => {
-      // Passo i valori presi da API
-      this.conftype     = infoconf.configurations.installation_type;
-      this.confname     = infoconf.configurations.installation_name;
-      this.confpath     = infoconf.configurations.import_base_path;
-      this.confinstall  = infoconf.configurations.installed;
-    });
+      this.getMobile();
 
-    // Mobile Device API
-    this.httpClient.get(this.ROOT_URL + '/mobiledevice', { headers })
-    .toPromise().then((mobileAPI:any) => {
-      // console.log(mobileAPI);
-      this.mobiles = mobileAPI.mobile_device;
-      // console.log(this.mobiles[0]);
-    });
+      this.getInfo();    
+    }
 
-    // Access Point API
-    this.httpClient.get(this.ROOT_URL + '/accesspoint', { headers })
-    .toPromise().then((apAPI:any) => {
-      this.taps = apAPI.total_access_point;
-      this.aps = apAPI.access_point;
-    });
+    // -------- API Requests -------------------------------------------------------------------------------------------------------------------------------------
+    // Informazioni Installazione
+    getInstallation() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/system/configuration', { headers })
+      .toPromise().then((data:any) => {
+        this.varInstallation = data.configurations;
+      });
+    }
+    // Templates
+    getItemTypeTemplate(tid: number){
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/template/itemtype/' + tid, { headers })
+      .toPromise().then((data:any) => {
+        this.templates = data.templates;
+      });
+    }
 
-    // Item Type API
-    this.httpClient.get(this.ROOT_URL + '/item/type', { headers })
-    .toPromise().then((itemtAPI:any) => {
-      this.titems_t = itemtAPI.total_itemtype;
-      this.items_t = itemtAPI.itemtype;
-    });
+    // Display & Etichette
+    getDisplay() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/labelinfo/statistics', { headers })
+      .toPromise().then((data:any) => {
+        this.varLabelStat = data;
+      });
+    }
+    // Total Matches
+    getTotalMatch() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/matching', { headers })
+      .toPromise().then((data:any) => {
+        this.varTotMatch = data.total_matching;
+      });
+    }
+    // Access Point
+    getAps() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/accesspoint', { headers })
+      .toPromise().then((data:any) => {
+        this.varAps = data;
+      });
+    }
 
-    // Import API
-    this.httpClient.get(this.ROOT_URL + '/import/queue')
-    .toPromise().then((impAPI:any) => {
-      this.timports = impAPI.total_importqueue;
-      this.imports = impAPI.importqueue;
-    });
+    // Articoli
+    getItems() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/item/type', { headers })
+      .toPromise().then((data:any) => {
+        this.varItem = data;
+      });
+    }
+    // Totale Articoli
+    getTotItem() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/item/concrete', { headers })
+      .toPromise().then((data:any) => {
+        this.varTotItem = data;
+      });
+    }
+    // Importazioni
+    getImport(){
+      this.httpClient.get(Global.URL_ROOT + '/import/queue')
+      .toPromise().then((data:any) => {
+        this.varImport = data;
+      });
+    }
 
-    // Total Items API
-    this.httpClient.get(this.ROOT_URL + '/item/concrete', { headers })
-    .toPromise().then((tot_item:any) => {
-      this.titem = tot_item.total_items;
-    });
 
-    // Total Matches API
-    this.httpClient.get(this.ROOT_URL + '/matching', { headers })
-    .toPromise().then((tot_match:any) => {
-      this.tmatch = tot_match.total_matching;
-    });
-  }
+    // Mobile Device
+    getMobile() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/mobiledevice', { headers })
+      .toPromise().then((mobileAPI:any) => {
+        this.varMobile = mobileAPI;
+      });
+    }
 
-  getItemTypeTemplate(tid: number){
-    // Header generale
-    let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
 
-    // Elenco Template
-    this.httpClient.get(this.ROOT_URL + '/template/itemtype/' + tid, { headers })
-    .toPromise().then((data:any) => {
-      // Passo i valori presi da API
-      this.templates = data.templates;
-      console.log(this.templates)
-    });
-
-  }
+    // Info Sistema
+    getInfo() {
+      let headers = new HttpHeaders().set('apikey', localStorage.getItem('apikey'));
+      this.httpClient.get(Global.URL_ROOT + '/system', { headers })
+      .toPromise().then((data:any) => {
+        this.system_info = data;
+      });
+    }
+  
 }
---
+++ src/app/variables/global.ts
@@ -0,0 +1,3 @@
+export class Global {
+    public static URL_ROOT: string = "http://pietro-test.dlinkddns.com:10082/api";
+}
